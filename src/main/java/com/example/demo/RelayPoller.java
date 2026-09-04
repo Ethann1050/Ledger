90bc63dac62d63c558c3d3a341a33a85985cdcc5
e <eneylonlwin@gmail.com>
2026-09-04 01:36:43 +0100

    finished the kafka poller and sending and recieving from it

--- src/main/java/com/example/demo/RelayPoller.java
+++ src/main/java/com/example/demo/RelayPoller.java
@@ -1,45 +1,49 @@
 package com.example.demo;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.LockModeType;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.scheduling.annotation.EnableScheduling;
+
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Component
 public class RelayPoller {
 
     private final OutboxRepo outboxRepo;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public RelayPoller(OutboxRepo outboxRepo) {
+    public RelayPoller(OutboxRepo outboxRepo, KafkaTemplate<String,String> kafkaTemplate) {
         this.outboxRepo = outboxRepo;
+        this.kafkaTemplate=kafkaTemplate;
     }
 
 
     @Scheduled(fixedDelay=10000)
-    @Transactional
-    public void checkForPending(){
+    public void checkForPending()  {
         List<Outbox> pendingOutboxes=outboxRepo.findPendingBatch(10);
         if (!pendingOutboxes.isEmpty()){
             for (Outbox outbox : pendingOutboxes){
-                sendToKafka(outbox);
-                updateOutboxStatus(outbox, OutboxStatus.SENT);
+                try {
+                    sendToKafka(outbox);
+                    updateOutboxStatus(outbox,OutboxStatus.SENT);
+                } catch (Exception e){
+                    System.out.println("Failed to send outbox entry ID {} to Kafka");
+                    updateOutboxStatus(outbox, OutboxStatus.FAILED);
+                }
+
+
+
             }
         }
     }
 
-    private void sendToKafka(Outbox outbox){
-
+    private void sendToKafka(Outbox outbox) throws ExecutionException, InterruptedException {
+        kafkaTemplate.send("ledger-events",outbox.getactionId(),outbox.getPayload()).get();
     }
 
-    public void receiveFromKafka(){}
-
-
-    private void updateOutboxStatus(Outbox outbox, OutboxStatus status){
+    private void updateOutboxStatus(Outbox outbox ,OutboxStatus status){
         outbox.setStatus(status);
         outboxRepo.save(outbox);
 
